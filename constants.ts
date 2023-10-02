253d7585b3ccfd733b98d83e887aa53353cefb8c
Andre Myrick <aMyrick.820@gmail.com>
2023-10-02 12:23:07 -0400

    define constants

---
+++ constants.ts
@@ -0,0 +1,10 @@
+export const LOW_COST_TRAVEL_DAY = 45
+export const HIGH_COST_TRAVEL_DAY = 55
+export const LOW_COST_FULL_DAY = 75
+export const HIGH_COST_FULL_DAY = 85
+
+export const COST_FLAG_INDEX = 0
+export const START_DATE_INDEX = 1
+export const END_DATE_INDEX = 2
+export const SINGLE_PROJECT_ARG_NUM = 3
+export const VALID_COST_FLAGS = ["-l", "-lc", "--low", "-h", "-hc", "--high"]
